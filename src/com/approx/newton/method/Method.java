ecfcfacf8cfe2ef5b995e9772d611ddfd33ae7b4
Andrew <SirDratuti>
2021-06-08 16:55:34 +0300

    deleted print in method interface

--- src/com/approx/newton/method/Method.java
+++ src/com/approx/newton/method/Method.java
@@ -8,11 +8,5 @@ import com.approx.newton.utils.MethodStats;
 public interface Method extends Printable {
 
     MethodStats solve(final Vector values, final Function function, final double eps);
-
-    default void print(final Vector x) {
-        for (int i = 0; i < x.size(); i++) {
-            System.out.print(x.get(i) + " ");
-        }
-        System.out.println();
-    }
+    
 }
